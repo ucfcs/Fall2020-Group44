6f500f87f98059ebb4d0faf10062815d6be19e9f
Kenny <kennygperez@gmail.com>
2021-01-24 22:19:46 -0500

    Added SVG module and icon component

---
+++ mobile/src/components/Icon.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Svg, { Path } from 'react-native-svg';
+
+function Icon({ type }: IconProps) {
+	return (
+		<Svg width='24' height='24' fill='none' viewBox='0 0 24 24'>
+			<Path fill='#000' d='M21 6H3v2h18V6zM21 16H3v2h18v-2zM21 11H3v2h18v-2z' />
+		</Svg>
+	);
+}
+
+export default Icon;
