0e4b7becbb3136cf3516a7e6e6b0479d1cdbaca3
Davis Goff <davisparkergoff@gmail.com>
2021-01-28 16:51:24 -0500

    Add routing to present and display poll

--- frontend/src/components/present-poll/present-preview.tsx
+++ frontend/src/components/present-poll/present-preview.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import './present-preview.scss'
 
 const PresentPreview = () => {
@@ -6,7 +6,7 @@ const PresentPreview = () => {
 		<div className="present-preview">
 			<img className="present-preview-logo" src="/img/logo.svg"/>
 			<h2>{"Who was the first president of the United States and also what would you do if you met him and also this is really long?"}</h2>
-			<button className="start-button">{"Start Poll >"}</button>
+			<button className="start-button"><Link to="/poll/display">Start Poll &gt;</Link></button>
 			<button className="skip-button">{"Skip to Next Poll >>"}</button>
 			<p className="helper-text">Click Start Poll to begin collecting responses</p>
 		</div>
