e9190eb6fc66266ec325456ff8790070d62060ea
Toan Vu <vutoan1245@gmail.com>
2021-01-28 15:12:06 -0500

    Add missing parameters error for folder endpoints

--- backend/src/folder.ts
+++ backend/src/folder.ts
@@ -3,14 +3,24 @@ const Folder = require('../model/Folder')
 const querystring = require('querystring')
 
 const mockUserid = 1
-const mockCourseId = 1
 
 const getFolder = async (event?: APIGatewayEvent): Promise<ProxyResult> => {
+    const params = event?.queryStringParameters
+
+    if (!params?.courseId) {
+        return {
+            statusCode: 400,
+            body: JSON.stringify({
+                message: 'Missing parameters',
+            }),
+        }
+    }
+
     await Folder.sync()
     return Folder.findAll({
         where: {
             userId: mockUserid,
-            courseId: event?.queryStringParameters?.courseId,
+            courseId: params?.courseId,
         },
     })
         .then((result: Array<Object>) => {
@@ -30,12 +40,22 @@ const getFolder = async (event?: APIGatewayEvent): Promise<ProxyResult> => {
 
 const newFolder = async (event?: APIGatewayEvent): Promise<ProxyResult> => {
     const body = querystring.parse(event?.body)
+    const params = event?.queryStringParameters
+
+    if (!params?.courseId) {
+        return {
+            statusCode: 400,
+            body: JSON.stringify({
+                message: 'Missing parameters',
+            }),
+        }
+    }
 
     await Folder.sync()
     return Folder.create({
         name: body?.name,
         userId: mockUserid,
-        courseId: body?.courseId,
+        courseId: params?.courseId,
     })
         .then(() => {
             return {
@@ -51,9 +71,22 @@ const newFolder = async (event?: APIGatewayEvent): Promise<ProxyResult> => {
 
 const updateFolder = async (event?: APIGatewayEvent): Promise<ProxyResult> => {
     const body = querystring.parse(event?.body)
+    const params = event?.queryStringParameters
+
+    if (!params?.folderId) {
+        return {
+            statusCode: 400,
+            body: JSON.stringify({
+                message: 'Missing parameters',
+            }),
+        }
+    }
 
     await Folder.sync()
-    return Folder.update({ name: body.name }, { where: { id: body.folderId } })
+    return Folder.update(
+        { name: body.name },
+        { where: { id: params?.folderId } }
+    )
         .then(() => {
             return {
                 status: 200,
@@ -67,10 +100,18 @@ const updateFolder = async (event?: APIGatewayEvent): Promise<ProxyResult> => {
 }
 
 const deleteFolder = async (event?: APIGatewayEvent): Promise<ProxyResult> => {
-    const body = querystring.parse(event?.body)
+    const params = event?.queryStringParameters
 
+    if (!params?.folderId) {
+        return {
+            statusCode: 400,
+            body: JSON.stringify({
+                message: 'Missing parameters',
+            }),
+        }
+    }
     await Folder.sync()
-    return Folder.destroy({ where: { id: body.folderId } })
+    return Folder.destroy({ where: { id: params?.folderId } })
         .then(() => {
             return {
                 status: 200,
