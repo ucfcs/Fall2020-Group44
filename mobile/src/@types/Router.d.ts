9927235efb556218c8aa30784d293319939a0489
Kenny <kennygperez@gmail.com>
2021-01-30 20:13:34 -0500

    Added router types

---
+++ mobile/src/@types/Router.d.ts
@@ -0,0 +1,13 @@
+type PollStackTree = {
+	Home: undefined;
+	Polls: undefined;
+};
+
+type SettingsStackTree = {
+	Settings: undefined;
+};
+
+type RootTree = {
+	Polls: undefined;
+	Settings: undefined;
+};
