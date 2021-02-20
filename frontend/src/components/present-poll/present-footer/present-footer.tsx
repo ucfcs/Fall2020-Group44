afd497836f25eb7d06bcfc6188d07f5c02e2566d
Weining Li <li.weining@knights.ucf.edu>
2021-02-19 20:49:39 -0500

    + present poll functionalities

--- frontend/src/components/present-poll/present-footer/present-footer.tsx
+++ frontend/src/components/present-poll/present-footer/present-footer.tsx
@@ -1,16 +1,130 @@
-import React, { ReactElement } from "react";
-import FooterQuestion from "../footer-question/footer-question";
+import React, {
+  FocusEvent,
+  MouseEvent,
+  ReactElement,
+  useState,
+  useContext,
+} from "react";
+import { store } from "../../../store";
+import {
+  DragDropContext,
+  Droppable,
+  Draggable,
+  DropResult,
+} from "react-beautiful-dnd";
 import "./present-footer.scss";
 
+interface PollQuestion {
+  title: string;
+  question: string;
+  type: string;
+  choices: string[];
+  correct: number;
+}
+
+function useForceUpdate() {
+  const [value, setValue] = useState(0); // integer state
+  return () => setValue((value) => value + 1); // update the state to force render
+}
+
 const PresentFooter = (): ReactElement => {
-  const questions = ["Q1", "Q2", "Q3"];
+  const global = useContext(store) as any;
+  const state = global.state;
+
+  const forceUpdate = useForceUpdate();
+
+  const [questions, setQuestions] = useState<PollQuestion[]>(state.poll);
+  const [questionsShown, setQuestionsShown] = useState(
+    new Array(state.poll.length).fill(false)
+  );
+
+  const handleDragEnd = (result: DropResult) => {
+    if (result.destination) {
+      const newQuestions: PollQuestion[] = questions;
+      const [srcQuestion] = questions.splice(result.source.index, 1);
+      newQuestions.splice(result.destination.index, 0, srcQuestion);
+      setQuestions(state.poll);
+    }
+  };
+
+  const handleShowTooltip = (
+    e: FocusEvent<HTMLLIElement> | MouseEvent<HTMLLIElement>,
+    index: number
+  ) => {
+    e.preventDefault();
+    e.stopPropagation();
+    const newShowArray = questionsShown;
+    newShowArray[index] = true;
+    setQuestionsShown(newShowArray);
+    forceUpdate();
+  };
+
+  const handleHideTooltip = (
+    e: FocusEvent<HTMLLIElement> | MouseEvent<HTMLLIElement>,
+    index: number
+  ) => {
+    e.preventDefault();
+    e.stopPropagation();
+    const newShowArray = questionsShown;
+    newShowArray[index] = false;
+    setQuestionsShown(newShowArray);
+    forceUpdate();
+  };
+
+  const handleDeleteQuestion = (index: number) => {
+    const newQuestions = questions.filter((_, i) => i !== index);
+    state.poll = newQuestions;
+    setQuestions(newQuestions);
+  };
 
   return (
-    <div className="present-footer">
-      {questions.map((question, index) => (
-        <FooterQuestion key={index} question={question} />
-      ))}
-    </div>
+    <DragDropContext onDragEnd={handleDragEnd}>
+      <Droppable droppableId="present-footer" direction="horizontal">
+        {(provided) => (
+          <ul
+            className="present-footer"
+            {...provided.droppableProps}
+            ref={provided.innerRef}
+          >
+            {questions.map((question, index) => (
+              <Draggable key={index} draggableId={index + ""} index={index}>
+                {(provided) => (
+                  <li
+                    className="footer-question"
+                    ref={provided.innerRef}
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}
+                    onFocus={(e) => handleShowTooltip(e, index)}
+                    onMouseEnter={(e) => handleShowTooltip(e, index)}
+                    onBlur={(e) => handleHideTooltip(e, index)}
+                    onMouseLeave={(e) => handleHideTooltip(e, index)}
+                  >
+                    <div
+                      className={`footer-tooltip ${
+                        questionsShown[index] ? "show" : ""
+                      }`}
+                    >
+                      {question.title}
+                    </div>
+                    <img src="/img/logo.svg" />
+                    <p>
+                      {"Q" + (index + 1)} {question.title}
+                    </p>
+                    <button
+                      className="delete"
+                      onClick={() => handleDeleteQuestion(index)}
+                    >
+                      X
+                    </button>
+                  </li>
+                )}
+              </Draggable>
+            ))}
+            {provided.placeholder}
+          </ul>
+        )}
+      </Droppable>
+    </DragDropContext>
   );
 };
 
--- frontend/src/components/present-poll/present-preview/present-preview.tsx
+++ frontend/src/components/present-poll/present-preview/present-preview.tsx
@@ -1,20 +1,25 @@
-import React, { ReactElement } from "react";
+import React, { ReactElement, useContext } from "react";
 import { Link } from "react-router-dom";
+import { store } from "../../../store";
 import "./present-preview.scss";
 
 const PresentPreview = (): ReactElement => {
+  const global = useContext(store) as any;
+  const state = global.state;
+
   return (
     <div className="present-preview">
       <img className="present-preview-logo" src="/img/logo.svg" />
-      <h2>
-        {
-          "Who was the first president of the United States and also what would you do if you met him and also this is really long?"
-        }
-      </h2>
+      <h2>{state.poll[0] ? state.poll[0].title : ""}</h2>
       <button className="start-button">
         <Link to="/poll/display">Start Poll &gt;</Link>
       </button>
-      <button className="skip-button">{"Skip to Next Poll >>"}</button>
+      <button
+        className="skip-button"
+        onClick={() => console.log("to be removed????")}
+      >
+        {"Skip to Next Poll >>"}
+      </button>
       <p className="helper-text">
         Click Start Poll to begin collecting responses
       </p>
