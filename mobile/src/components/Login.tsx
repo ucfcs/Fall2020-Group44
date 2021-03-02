4723814f281707c4cc07432f71094181c9a52551
Kenny <kennygperez@gmail.com>
2021-03-01 22:49:01 -0500

    Blocked by api

--- mobile/src/components/Login.tsx
+++ mobile/src/components/Login.tsx
@@ -5,7 +5,14 @@ import React, {
 	useState,
 	useContext,
 } from 'react';
-import { StyleSheet, View, SafeAreaView, Linking } from 'react-native';
+import {
+	StyleSheet,
+	View,
+	SafeAreaView,
+	Linking,
+	Alert,
+	ActivityIndicator,
+} from 'react-native';
 
 import { GOLD } from '../libs/colors';
 import { Button } from './Button';
@@ -26,9 +33,21 @@ const styles = StyleSheet.create({
 		width: '100%',
 		height: 320,
 	},
+	overlay: {
+		position: 'absolute',
+		top: 0,
+		left: 0,
+		width: '100%',
+		height: '100%',
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		backgroundColor: 'rgba(0, 0, 0, 0.25)',
+	},
 });
 
 export const Login: FunctionComponent = () => {
+	const [isReadingAuthLink, setIsReadingAuthLink] = useState(false);
 	const { state } = useContext(AppContext);
 	const handlePress = useCallback(async () => {
 		// Checking if the link is supported for links with custom URL scheme.
@@ -44,25 +63,53 @@ export const Login: FunctionComponent = () => {
 	}, [state.url]);
 
 	useEffect(() => {
-		Linking.addEventListener('url', (event) => {
+		Linking.addEventListener('url', async (event) => {
 			const { url } = event;
+
+			// assume this run is good
+			setIsReadingAuthLink(true);
 
 			// make sure its set
 			if (!url) {
+				setIsReadingAuthLink(false);
+				Alert.alert('Error in auth', 'Error in auth stage 1');
 				return;
 			}
 
-			console.log(url);
+			// param section
+			const temp1 = url.split('?')[1];
+
+			if (!temp1) {
+				setIsReadingAuthLink(false);
+				Alert.alert('Error in auth', 'Error in auth stage 2');
+				return;
+			}
+
+			// get value after the ket "token="
+			const token = temp1.split('=')[1];
+			console.log(token);
+
+			// make call to server for user info
+			// then save it to the app state context
 		});
+
+		// on unmount remove all listeners
 		return () => Linking.removeAllListeners('url');
 	}, []);
 
 	return (
-		<SafeAreaView style={styles.safeArea}>
-			<View style={styles.container}>
-				<View style={styles.logoBox} />
-				<Button text='Sign In' type='secondary' onPress={handlePress} />
-			</View>
-		</SafeAreaView>
+		<>
+			<SafeAreaView style={styles.safeArea}>
+				<View style={styles.container}>
+					<View style={styles.logoBox} />
+					<Button text='Sign In' type='secondary' onPress={handlePress} />
+				</View>
+			</SafeAreaView>
+			{isReadingAuthLink && (
+				<View style={styles.overlay}>
+					<ActivityIndicator color='white' size='large' />
+				</View>
+			)}
+		</>
 	);
 };
