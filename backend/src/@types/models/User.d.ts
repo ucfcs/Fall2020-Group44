f1c9f68507d500151937c3e85f122fe5dbe59186
Kenny <kennygperez@gmail.com>
2021-03-22 21:15:07 -0400

    Updated user model

--- backend/src/@types/models/User.d.ts
+++ backend/src/@types/models/User.d.ts
@@ -3,14 +3,12 @@ type UserId = number;
 interface UserAttributes {
 	id: UserId;
 	canvasId: number;
-	fullName: string | null;
 	token: string | null;
 	refreshToken: string | null;
 }
 
 interface UserCreationAttributes {
 	canvasId: number;
-	fullName?: string | null;
 	token?: string | null;
 	refreshToken?: string | null;
 }
