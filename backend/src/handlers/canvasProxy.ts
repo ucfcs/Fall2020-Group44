009ed1ba115b96c6d4769217e8431393360b6305
Kenny <kennygperez@gmail.com>
2021-03-29 00:13:17 -0400

    Updated backend to support refresh token

--- backend/src/handlers/canvasProxy.ts
+++ backend/src/handlers/canvasProxy.ts
@@ -1,12 +1,13 @@
 import { APIGatewayProxyHandler } from 'aws-lambda';
 import fetch from 'node-fetch';
 
-import { verifyAuthentication } from '../util/auth';
+import { refreshUserToken, verifyAuthentication } from '../util/auth';
 import responses from '../util/api/responses';
 
 export const handler: APIGatewayProxyHandler = async (event) => {
 	// decode the jwt token
 	const user = verifyAuthentication(event.headers);
+	let newJWT;
 
 	if (!user) {
 		return responses.unauthorized();
@@ -33,11 +34,28 @@ export const handler: APIGatewayProxyHandler = async (event) => {
 			method: body.method,
 			headers: {
 				Authorization: `Bearer ${user.token}`,
+				'Content-Type': 'application/json',
 			},
 		});
 		const payload = await res.json();
 
-		return responses.ok({ payload });
+		// if the request returns 401 then the users token has
+		// most likely expired
+		if (res.status === 401) {
+			// lets refresh the users token
+			newJWT = await refreshUserToken(user);
+		}
+
+		// if a newJWT was generated we need to include it
+		// in the response object
+		if (newJWT) {
+			return responses.ok({
+				payload,
+				newJWT,
+			});
+		} else {
+			return responses.ok({ payload });
+		}
 	} catch (error) {
 		console.error(error);
 		return responses.internalServerError();
--- backend/src/util/auth.ts
+++ backend/src/util/auth.ts
@@ -1,4 +1,6 @@
 import { APIGatewayProxyEventHeaders } from 'aws-lambda';
+import fetch from 'node-fetch';
+
 import { User } from '../models/User';
 import { decode, encode } from './token';
 
@@ -53,21 +55,67 @@ export async function userAuthFlowGetToken(
 export function verifyAuthentication(
 	headers: APIGatewayProxyEventHeaders
 ): UserAttributes | null {
-	const bearer = headers['Authorization'];
-
-	if (!bearer) {
-		return null;
-	}
-
-	const token = bearer.substr(7);
-
-	if (!token) {
-		return null;
-	}
-
 	try {
+		const bearer = headers['Authorization'];
+
+		if (!bearer) {
+			return null;
+		}
+
+		const token = bearer.substr(7);
+
+		if (!token) {
+			return null;
+		}
+
 		return decode<UserAttributes>(token);
 	} catch (error) {
+		return null;
+	}
+}
+
+export async function refreshUserToken(
+	user: UserAttributes
+): Promise<string | null> {
+	try {
+		// request a new token from Canvas
+		const res = await fetch(`${process.env.CANVAS_URL}/login/oauth2/token`, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({
+				grant_type: 'refresh_token',
+				client_id: process.env.CANVAS_ID,
+				client_secret: process.env.CANVAS_KEY,
+				redirect_uri: process.env.CANVAS_REDIRECT,
+				refresh_token: user.refreshToken,
+			}),
+		});
+		const payload = await res.json();
+
+		if (res.status === 200) {
+			// now lets update our user token columns
+			await User.update(
+				{
+					token: payload.access_token,
+				},
+				{
+					where: {
+						id: user.id,
+					},
+					limit: 1,
+				}
+			);
+
+			user.token = payload.access_token;
+
+			return encode(user);
+		} else {
+			return null;
+		}
+	} catch (error) {
+		console.error(error);
 		return null;
 	}
 }
