71490fc5b7db2c974bdf1b37ac5ca58105b12006
Kenny <kennygperez@gmail.com>
2021-03-28 22:05:11 -0400

    Fixed colors.ts

--- mobile/src/libs/colors.ts
+++ mobile/src/libs/colors.ts
@@ -1,8 +1,7 @@
-import { exp } from 'react-native-reanimated';
-
 export const GOLD = '#FFC904';
 export const WHITE = '#E5E5E5';
 export const BLACK = '#000000';
 export const GRAY_2 = '#666666';
 export const GRAY_3 = '#999999';
 export const GRAY_4 = '#CCCCCC';
+export const GREEN = '#2B8600';
