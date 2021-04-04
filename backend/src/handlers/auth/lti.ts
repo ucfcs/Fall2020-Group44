41e0496e101111fb5894f4be7829a2e6433f6f4a
Toan Vu <vutoan1245@gmail.com>
2021-04-04 10:34:32 -0400

    Parse data from LTI provider

--- backend/src/handlers/auth/lti.ts
+++ backend/src/handlers/auth/lti.ts
@@ -3,62 +3,74 @@ import {
 	APIGatewayProxyHandler,
 	ProxyResult,
 } from 'aws-lambda';
+import uuid from 'uuid';
 import response from '../../util/api/responses';
 import querystring from 'querystring';
+import { userAuthFlowGetToken } from '../../util/auth';
 
 /**
  * @see http://localhost:3000/dev/api/v1/auth/lti
  */
-export const launch: APIGatewayProxyHandler = async () => {
+export const launch: APIGatewayProxyHandler = async (
+	event: APIGatewayEvent
+) => {
 	// do we have a user with that info
+	console.log(querystring.parse(event.body as string));
 
 	return response.movedPermanently(process.env.SITE_BASE_URL as string);
 };
 
-// https://canvas.instructure.com/doc/api/file.lti_dev_key_config.html
-// Step 1: Login Initiation
+/**
+ *
+ * LTI Launch: https://canvas.instructure.com/doc/api/file.lti_dev_key_config.html
+ *
+ */
+
+// Step 2: Authentication Request
 export const login = async (event: APIGatewayEvent): Promise<ProxyResult> => {
 	console.log('--------------------\nlogin');
 
-	// TODO: generate v4 uuid;
-	const state = 'f0e41f9d-dc90-471b-a651-636ae26df124';
-	const nonce = '75fd1447-cd57-4423-bc3b-a11040b9adb9';
+	const state = uuid.v4();
+	const nonce = uuid.v4();
 
 	const body = querystring.parse(event.body as string);
-	console.log('body', body);
 
 	const url =
-		`${process.env.CANVAS_URL}/api/lti/authorize_redirect` +
-		'?response_type=id_token' +
-		'&scope=openid' +
-		'&login_hint=' +
-		body.login_hint +
-		'&lti_message_hint=' +
-		body.lti_message_hint +
-		'&state=' +
-		state +
-		'&redirect_uri=' +
-		encodeURIComponent(process.env.LTI_REDIRECT_URL as string) +
-		'&client_id=' +
-		process.env.CANVAS_LTI_CLIENT_ID +
-		'&nonce=' +
-		nonce +
-		'&prompt=none' +
-		'&response_mode=form_post' +
-		'&output=embed';
+		`${process.env.CANVAS_URL}/api/lti/authorize_redirect?` +
+		querystring.encode({
+			response_type: 'id_token',
+			scipe: 'openid',
+			login_hint: body.login_hint,
+			lti_message_hint: body.lti_message_hint,
+			state: state,
+			redirect_uri: process.env.LTI_REDIRECT_URL,
+			nonce: nonce,
+			prompt: 'none',
+			response_mode: 'form_post',
+		});
 
-	console.log('LTI JWT login init; redirecting to: ' + url);
+	console.log('LTI JWT login init; redirecting to:', url);
 
 	return response.movedPermanently(url);
 };
 
 // Step 4: Resource Display
 export const lti13 = async (event: APIGatewayEvent): Promise<ProxyResult> => {
-	console.log('--------------------\nti13');
+	console.log('--------------------\nlti13');
 
 	const body = querystring.parse(event.body as string);
-	console.log(body);
-	// TODO: verify token
 
-	return response.movedPermanently(process.env.SITE_BASE_URL as string);
+	// Parse and store payload data from launch
+	const id_token = body.id_token as string;
+	const parts = id_token.split('.');
+	const tokenBody = JSON.parse(Buffer.from(parts[1], 'base64').toString());
+
+	const courseData =
+		tokenBody['https://purl.imsglobal.org/spec/lti/claim/custom'];
+
+	const token = await userAuthFlowGetToken(courseData.userid, '', '');
+
+	return response.movedPermanently(
+		`${process.env.SITE_BASE_URL}/course/${courseData.courseid}?token=${token}` as string
+	);
 };
