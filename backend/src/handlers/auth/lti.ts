ca7897756f7bed274c31b30eed0dab7af53d68d4
Toan Vu <vutoan1245@gmail.com>
2021-04-14 16:58:54 -0400

    Add endpoint to export grades to Canvas

--- backend/src/handlers/auth/lti.ts
+++ backend/src/handlers/auth/lti.ts
@@ -45,6 +45,7 @@ export const launch: APIGatewayProxyHandler = async (
 			'url:POST|/api/v1/courses/:course_id/assignments',
 			'url:GET|/api/v1/courses/:course_id/students',
 			'url:POST|/api/v1/courses/:course_id/assignments/:assignment_id/submissions/update_grades',
+			'url:PUT|/api/v1/courses/:course_id/assignments/:assignment_id/submissions/:user_id',
 		];
 		const url =
 			`${process.env.CANVAS_URL}/login/oauth2/auth?` +
--- backend/src/util/canvas.ts
+++ backend/src/util/canvas.ts
@@ -55,3 +55,73 @@ export const getStudents = async (
 		return Promise.reject(error);
 	}
 };
+
+export const createAssignment = async (
+	userId: number,
+	courseId: number,
+	name: string,
+	maxPoint: number
+): Promise<number> => {
+	try {
+		const token = await getToken(userId);
+
+		const res = await fetch(
+			`${process.env.CANVAS_URL}/api/v1/courses/${courseId}/assignments?` +
+				querystring.encode({
+					'assignment[name]': name,
+					'assignment[points_possible]': maxPoint,
+					'assignment[published]': true,
+				}),
+			{
+				method: 'POST',
+				headers: {
+					Authorization: `Bearer ${token}`,
+				},
+			}
+		);
+
+		const data = await res.json();
+
+		return Number(data.id);
+	} catch (error) {
+		console.log('Error while creating assignment:', error);
+		return Promise.reject(error);
+	}
+};
+
+export const postGrades = async (
+	userId: number,
+	courseId: number,
+	assignmentId: number,
+	grades: StudentGrade[]
+): Promise<void> => {
+	try {
+		const token = await getToken(userId);
+
+		await Promise.all(
+			grades.map(async (grade) => {
+				await fetch(
+					`${process.env.CANVAS_URL}/api/v1/courses/${courseId}/assignments/${assignmentId}/submissions/${grade.id}?` +
+						querystring.encode({
+							'submission[posted_grade]': grade.points,
+						}),
+
+					{
+						method: 'PUT',
+						headers: {
+							Authorization: `Bearer ${token}`,
+						},
+					}
+				);
+			})
+		);
+	} catch (error) {
+		console.log('Error while posting grade:', error);
+		return Promise.reject(error);
+	}
+};
+
+interface StudentGrade {
+	id: number;
+	points: number;
+}
