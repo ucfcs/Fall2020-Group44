a134654ed6a881f4c3b5b32167b1e6621da7e596
Lawrence Oks <ljoks0727@gmail.com>
2021-04-18 21:53:57 -0400

    add colorful bars and text styling

--- frontend/src/components/gradebook/gradebook.tsx
+++ frontend/src/components/gradebook/gradebook.tsx
@@ -21,6 +21,9 @@ import HomeHeader from "../home-header/home-header";
 import { store } from "../../store";
 import { catchError, getCourseGrades } from "../../util/api";
 
+const RED = 0.5;
+const YELLOW = 0.75;
+
 const Gradebook = (): ReactElement => {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const global = useContext(store) as any;
@@ -70,6 +73,12 @@ const Gradebook = (): ReactElement => {
     );
   };
 
+  const getBackgroundColor = (percentage: number): string => {
+    if (percentage < RED) return "#FF0033";
+    else if (percentage < YELLOW) return "#FFC904";
+    else return "#00CA51";
+  };
+
   const exportToCanvas = () => {
     dispatch({ type: "open-export-modal" });
   };
@@ -92,13 +101,14 @@ const Gradebook = (): ReactElement => {
             <table>
               <thead>
                 <tr>
-                  <th>Student</th>
-                  <th>Totals</th>
+                  <th className="header-text first-column">Student</th>
+                  <th className="header-text">Totals</th>
 
                   {sessions.map((session: BasicSessionInfo, sIndex: number) => (
                     <th key={sIndex} className="session-name">
-                      <Link to={`/gradebook/${session.id}`}>
-                        {session.name}
+                      <div className="header-text">{session.name}</div>
+                      <Link className="expand" to={`/gradebook/${session.id}`}>
+                        {"View >"}
                       </Link>
                     </th>
                   ))}
@@ -106,17 +116,52 @@ const Gradebook = (): ReactElement => {
               </thead>
               <tbody>
                 <tr className="averages">
-                  <td>Class Average</td>
-                  <td className="align-right">
-                    {classAverage.points.toFixed(2)}/
-                    {classAverage.maxPoints.toFixed(2)}
+                  <td className="first-column averages-text">Class Average</td>
+                  <td className="align-right averages-text">
+                    <div>
+                      {classAverage.points.toFixed(2)} /{" "}
+                      {classAverage.maxPoints.toFixed(2)}
+                    </div>
+                    <div className="bar">
+                      <div
+                        className="bar-value"
+                        style={{
+                          width:
+                            (classAverage.points / classAverage.maxPoints) *
+                              100 +
+                            "%",
+                          backgroundColor: getBackgroundColor(
+                            classAverage.points / classAverage.maxPoints
+                          ),
+                        }}
+                      ></div>
+                    </div>
                   </td>
-                  {sessions.map((session: BasicSessionInfo, sIndex: number) => (
-                    <td key={sIndex} className="session-average align-right">
-                      {session.SessionGrades[0].avgPoints.toFixed(2)} /
-                      {session.SessionGrades[0].maxPoints.toFixed(2)}
-                    </td>
-                  ))}
+                  {sessions.map((session: BasicSessionInfo, sIndex: number) => {
+                    const average = session.SessionGrades[0].avgPoints;
+                    const max = session.SessionGrades[0].maxPoints;
+                    return (
+                      <td
+                        key={sIndex}
+                        className="session-average align-right averages-text"
+                      >
+                        <div>
+                          {average.toFixed(2)} / {max.toFixed(2)}
+                        </div>
+                        <div className="bar">
+                          <div
+                            className="bar-value"
+                            style={{
+                              width: (average / max) * 100 + "%",
+                              backgroundColor: getBackgroundColor(
+                                average / max
+                              ),
+                            }}
+                          ></div>
+                        </div>
+                      </td>
+                    );
+                  })}
                 </tr>
                 {students.map((student: StudentSessionInfo, index: number) => (
                   <Student key={index} student={student} />
