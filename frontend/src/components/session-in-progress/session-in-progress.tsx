e7bb3c26ab91c9c78154cab25d855fcd708d60fd
Weining Li <li.weining@knights.ucf.edu>
2021-04-19 20:47:29 -0400

    scroll stuff

--- frontend/src/components/session-in-progress/session-in-progress.tsx
+++ frontend/src/components/session-in-progress/session-in-progress.tsx
@@ -118,16 +118,15 @@ const SessionInProgress = (): ReactElement => {
     <div className="session-in-progress">
       <PollHeader />
 
-      {/* <div className="grid"> */}
-      <SessionProgress
-        classSize={classSize}
-        responseCount={state.sessionQuestions[questionNumber].responseCount}
-      />
-
       <Sidebar questionCount={questions.length} />
-
-      <div className="content">{content()}</div>
-      {/* </div> */}
+      <div className="session-content">
+        <SessionProgress
+          classSize={classSize}
+          responseCount={state.sessionQuestions[questionNumber].responseCount}
+        />
+        <div className="content">{content()}</div>
+        {/* </div> */}
+      </div>
     </div>
   );
 };
